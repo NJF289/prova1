14af25c05be2262add23d899744ff3849c69776e
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-05-02 00:17:43 +0000

    fix:ajustsbanner

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,12 +1,12 @@
 export default function Content() {
     return (
-        <div className=" mt-10 border-t-4 border-[#D2D7D9] ">
+        <div className="mt-10 border-t-4 border-[#D2D7D9] ">
             <p className="font-bold text-left mt-8">Erat lacinia</p>
-            <p className="border-t-4 border-[#F56A6A] mt-3 max-w-28"></p>
+            <p className="mt-3 border-t-4 border-[#F56A6A] max-w-28"></p>
             <div className=" grid-cols-2 md:grid">
             
-            <div className=" text-justify mt-8 grid-cols-2 md:grid items-center">
-                <div className="max-w-28"> <img src="/img/spannn.png" alt="" /></div>
+            <div className="text-justify mt-8 grid-cols-2 md:grid ">
+                <div className="max-w-24 place-self-end mr-7"> <img src="/img/spannn.png" alt="" /></div>
                 <div>
                 <h1 className="font-bold">Portitor ullamcorper</h1>
                 <p className="text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam</p>
@@ -14,7 +14,7 @@ export default function Content() {
                     </div> 
                
                 <div className="text-justify mt-8 grid-cols-2 md:grid">
-                <div className="max-w-28"> <img src="/img/span1.png" alt="" /></div>
+                <div className="max-w-24 place-self-end md:mr-7"> <img src="/img/span1.png" alt="" /></div>
                 <div>
                 <h1 className="font-bold">Sapien veroeros</h1>
                 <p className="text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
@@ -22,7 +22,7 @@ export default function Content() {
                     </div> 
                 
                 <div className="text-justify mt-8 grid-cols-2 md:grid">
-                <div className="max-w-28"> <img src="/img/span2.png" alt="" /></div>
+                <div className="max-w-24 place-self-end md:mr-7"> <img src="/img/span2.png" alt="" /></div>
                 <div>
                 <h1 className="font-bold">Quam lorem ipsum</h1>
                 <p className="text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
@@ -30,7 +30,7 @@ export default function Content() {
                     </div> 
 
                 <div className="text-justify mt-8 mr-4 grid-cols-2 md:grid">
-                <div className="max-w-24"> <img src="/img/span3.png" alt="" /></div>
+                <div className="max-w-24 md:place-self-end md:mr-7"> <img src="/img/span3.png" alt="" /></div>
                 <div>
                 <h1 className="font-bold">Sed magna finibus</h1>
                 <p className="text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="flex justify-between mt-8 ">
+        <div className=" justify-between mt-8 ">
             <h1><span className="font-bold"> News </span> by UTFPR</h1>
         </div>
     )
