0f315e4d7850bcaf6c9079e9b4d6e7d51641e79b
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-05-02 02:25:12 +0000

    fix:Bannerend

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -3,35 +3,35 @@ export default function Content() {
         <div className="mt-10 border-t-4 border-[#D2D7D9] ">
             <p className="font-bold text-left mt-8">Erat lacinia</p>
             <p className="mt-3 border-t-4 border-[#F56A6A] max-w-28"></p>
-            <div className=" grid-cols-2 md:grid md:mt-16">
+            <div className=" grid-cols-2 md:grid md:mt-12">
             
-            <div className="text-justify mt-8 grid-cols-3 md:grid">
-                <div className="max-w-28 place-self-end mr-7 "> <img src="/img/spannn.png" alt="" /></div>
-                <div className="col-span-2">
+            <div className="text-justify mt-8 grid-cols-5 md:grid lg:grid-cols-3 ">
+                <div className="col-span-2 max-w-28 place-self-end mr-7 lg:col-span-1 "> <img src="/img/spannn.png" alt="" /></div>
+                <div className="col-span-3 lg:col-span-2">
                 <h1 className="font-bold">Portitor ullamcorper</h1>
                 <p className="mt-2 text-[#7F888F] ">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam</p>
                     </div>
                     </div> 
                
-                <div className="text-justify mt-8 grid-cols-3 md:grid">
-                <div className="max-w-24 place-self-end md:mr-7"> <img src="/img/span1.png" alt="" /></div>
-                <div className="col-span-2">
+                <div className="text-justify mt-8 grid-cols-5 md:grid lg:grid-cols-3 ">
+                <div className="col-span-2 max-w-28 place-self-end md:mr-7 lg:col-span-1"> <img src="/img/span1.png" alt="" /></div>
+                <div className="col-span-3 lg:col-span-2">
                 <h1 className="font-bold">Sapien veroeros</h1>
                 <p className="mt-2 text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
                     </div>
                     </div> 
                 
-                <div className="text-justify mt-8 grid-cols-3 md:grid">
-                <div className="max-w-24 place-self-end md:mr-7"> <img src="/img/span2.png" alt="" /></div>
-                <div className="col-span-2">
+                <div className="text-justify mt-8 grid-cols-5 md:grid lg:grid-cols-3">
+                <div className="col-span-2 max-w-28 place-self-end md:mr-7 lg:col-span-1"> <img src="/img/span2.png" alt="" /></div>
+                <div className="col-span-3 lg:col-span-2">
                 <h1 className="font-bold">Quam lorem ipsum</h1>
                 <p className="mt-2 text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
                     </div>
                     </div> 
 
-                <div className="text-justify mt-8 mr-4 grid-cols-3 md:grid">
-                <div className="max-w-24 md:place-self-end md:mr-7"> <img src="/img/span3.png" alt="" /></div>
-                <div className="col-span-2">
+                <div className="text-justify mt-8 mr-4 grid-cols-5 md:grid lg:grid-cols-3">
+                <div className="col-span-2 max-w-28 md:place-self-end md:mr-7 lg:col-span-1"> <img src="/img/span3.png" alt="" /></div>
+                <div className="col-span-3 lg:col-span-2">
                 <h1 className="font-bold">Sed magna finibus</h1>
                 <p className=" mt-2 text-[#7F888F]">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
                     </div> 
